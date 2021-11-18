34463c3e7e7ff5fcec0884bc566db2475801636b
Ryan Lee <91900654+ForSchool13372@users.noreply.github.com>
2021-11-18 16:32:01 -0500

    Update Unity.cpp

--- Unity.cpp
+++ Unity.cpp
@@ -5,11 +5,17 @@ public class Controller : MonoBehaviour
 {
     public float moveSpeed;
     Vector3 direction;
+    public Animator animator;
 
     private void Update()
     {
         float v = Input.GetAxisRaw("Vertical");
         float h = Input.GetAxisRaw("Horizontal");
+        bool up = false;
+        bool down = false;
+        bool left = false;
+        bool right = false;
+
 
 
         if (h != 0 || v != 0)
@@ -17,18 +23,38 @@ public class Controller : MonoBehaviour
             if (Input.GetKey(KeyCode.D))
             {
                 this.transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+                animator.SetBool("Up", false);
+                animator.SetBool("Down", false);
+                animator.SetBool("Right", true);
+                animator.SetBool("Left", false);
+                right = true;
             }
             else if (Input.GetKey(KeyCode.A))
             {
                 this.transform.Translate(-Vector3.right * moveSpeed * Time.deltaTime);
+                animator.SetBool("Up", false);
+                animator.SetBool("Down", false);
+                animator.SetBool("Right", false);
+                animator.SetBool("Left", true);
+                left = true;
             }
             else if (Input.GetKey(KeyCode.W))
             {
+                animator.SetBool("Down", false);
+                animator.SetBool("Up", true);
+                animator.SetBool("Right", false);
+                animator.SetBool("Left", false);
+                up = true;
                 this.transform.Translate(Vector3.up * moveSpeed * Time.deltaTime);
             }
             else if (Input.GetKey(KeyCode.S))
             {
                 this.transform.Translate(-Vector3.up * moveSpeed * Time.deltaTime);
+                animator.SetBool("Up", false);
+                animator.SetBool("Down", true);
+                animator.SetBool("Right", false);
+                animator.SetBool("Left", false);
+                down = true;
             }
         }
     }
